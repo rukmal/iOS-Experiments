adca2cd881f3b7f9f93736d02c0f7ca0297f17ae
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-13 18:08:42 -0700

    Added a Hello World app

---
+++ HelloWorld/HelloWorld/RWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RWAppDelegate.h
+//  HelloWorld
+//
+//  Created by Rukmal Weerawarana on 5/13/14.
+//  Copyright (c) 2014 Rukmal Weerawarana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/HelloWorld/RWViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWViewController.h
+//  HelloWorld
+//
+//  Created by Rukmal Weerawarana on 5/13/14.
+//  Copyright (c) 2014 Rukmal Weerawarana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWViewController : UIViewController
+
+@end
