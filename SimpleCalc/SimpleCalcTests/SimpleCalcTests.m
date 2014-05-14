349b48026de106c3f99227aa4568ed0b5e9ae11d
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-05-14 15:09:25 -0700

    Added an icon to HelloWorld

---
+++ SimpleCalc/SimpleCalcTests/SimpleCalcTests.m
@@ -0,0 +1,34 @@
+//
+//  SimpleCalcTests.m
+//  SimpleCalcTests
+//
+//  Created by Rukmal Weerawarana on 5/14/14.
+//  Copyright (c) 2014 Rukmal Weerawarana. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SimpleCalcTests : XCTestCase
+
+@end
+
+@implementation SimpleCalcTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
