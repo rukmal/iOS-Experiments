f97e43b5f878ec79b2761e18590f47d1d4103c53
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-06-03 15:00:30 -0700

    Re-added simple calc. Switched to Swift

---
+++ SimpleCalc/SimpleCalc/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SimpleCalc
+//
+//  Created by Rukmal Weerawarana on 6/3/14.
+//  Copyright (c) 2014 Rukmal Weerawarana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var operationPicker : UIPickerView
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
