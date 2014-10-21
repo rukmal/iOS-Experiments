44bf715e4155196b4411ea123a4714925eff9c32
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-10-21 02:17:46 -0700

    Finished hello world

--- HelloWorld/HelloWorld/ViewController.swift
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+var counter: Int = 0
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func triggerButton(sender: UIButton) {
+        counter += 1
+        var message: String = "You hello world'ed " + String(counter) + " times!"
+        var alert = UIAlertController(title: "Hello!", message: message, preferredStyle: .Alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+        var resetAction: UIAlertAction = UIAlertAction(title: "Reset", style: .Destructive, handler: { action in
+            counter = 0
+        })
+        alert.addAction(resetAction)
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 
 }
-
