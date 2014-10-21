ec63303a053cf4d9940e2bd44425a86a9cad880f
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2014-10-21 01:19:40 -0700

    Added single view applciation with sample println

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Rukmal Weerawarana on 10/21/14.
+//  Copyright (c) 2014 Rukmal Weerawarana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
